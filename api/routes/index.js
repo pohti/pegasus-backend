819ae936791ccdec40acd64ecdd4c7953836059a
Poh <minmarnoo@gmail.com>
2019-10-23 12:07:14 +0800

    heroku

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,25 +4,24 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-// PATHS
+// IMPORTING and SETTING UP PATHS
 const currentPath = path.join(__dirname)
-const loginRoutes = require(`${currentPath}/login`)
-const createUserRoutes = require(`${currentPath}/createUser`)
+const login = require(`${currentPath}/login`)
+const createUser = require(`${currentPath}/createUser`)
 const employerRoutes = require(`${currentPath}/employer/employerRouter`)
 const studentRoutes = require(`${currentPath}/student/studentRouter`)
 
 // ROUTING
-router.use('/login', loginRoutes)
-router.use('/createUser', createUserRoutes)
+router.post('/login', login)
+router.post('/createuser', createUser)
 
-//router.use('/employer/joblist', require(`${currentPath}/jobList`))
 router.use('/employer', employerRoutes)
 
 router.use('/student', studentRoutes)
 
 router.get('/', (req, res) => {
     console.log(`Received GET from ${currentPath}`)
-    res.send('Hello from Express Server')
+    res.send('Welcome to Pegasus API')
 })
 
 module.exports = router
