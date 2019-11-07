d5e4e5d39033470bd87a6e8101b8156d9ebae90a
Poh <minmarnoo@gmail.com>
2019-11-08 00:59:06 +0800

    heroku

--- api/routes/admin/adminRouter.js
+++ api/routes/admin/adminRouter.js
@@ -20,6 +20,15 @@ router.put('/editemployer/:id', editemployer)
 const editstudent = require('./editstudent')
 router.put('/editstudent/:id', editstudent)
 
+const deleteemployer = require('./deleteemployer')
+router.put('/deleteemployer/:id', deleteemployer)
+
+const deletestudent = require('./deletestudent')
+router.put('/deletestudent/:id', deletestudent)
+
+const deletejob = require('./deletejob')
+router.put('/deletejob/:id', deletejob)
+
 router.get('/', (req, res) => {
     res.send('hello from backend server, admin route')
 })
