c3c04ca9e3e170210190bca338899168bfa988de
Poh <minmarnoo@gmail.com>
2019-11-20 02:20:15 +0800

    heroku

--- api/routes/student/searchjob.js
+++ api/routes/student/searchjob.js
@@ -12,12 +12,13 @@ const searchjob = (req,res) => {
             throw err
         }
         else {
-            let queryString = `SELECT * FROM pegasus.job
-                                WHERE title LIKE '%${keyword}%'
-                                    OR industry LIKE '%${keyword}%'
-                                    OR description LIKE '%${keyword}%'
-                                    OR requiredskills LIKE '%${keyword}%'
+            let queryString = `SELECT pegasus.job.*, pegasus.employer.companyname 
+                                FROM pegasus.job
+                                JOIN pegasus.employer ON job.empid=employer.id
+                                WHERE job.title LIKE '%${keyword}%'                              
                                 LIMIT 50`
+
+
             connection.query(queryString, (err, rows, fields) => {
                 if(err) {
                     res.status(500).json({ message: err })
