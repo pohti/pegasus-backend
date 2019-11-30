a531ee6b340f7f11bc7c8522ab74939740399453
Poh <minmarnoo@gmail.com>
2019-11-30 11:51:19 +0800

    Update deletejob.js

--- api/routes/admin/deletejob.js
+++ api/routes/admin/deletejob.js
@@ -4,26 +4,26 @@ const deletejob = (req,res) => {
     const id = req.params.id
     //res.send('request to delete job, id: ' + id)
 
-    mypool.getConnection((err, connection) => {
-        if(err) {
-            connection.release()
-            console.log(`Error getting mysql_pool connection: ${err}`)
-            throw err
-        }
-        else {
+    // mypool.getConnection((err, connection) => {
+    //     if(err) {
+    //         connection.release()
+    //         console.log(`Error getting mysql_pool connection: ${err}`)
+    //         throw err
+    //     }
+    //     else {
             
-            let queryString1 = `DELETE FROM pegasus.jobapplication WHERE jobid=${id}`
-            connection.query(queryString1, (err) => {
-                if(err) {
-                    res.status(500).json({ message: err })
-                }
-                else {
-                    console.log('deleted from child tables')
-                }
-            })
-        }
-        connection.release()
-    })
+    //         let queryString1 = `DELETE FROM pegasus.jobapplication WHERE jobid=${id}`
+    //         connection.query(queryString1, (err) => {
+    //             if(err) {
+    //                 res.status(500).json({ message: err })
+    //             }
+    //             else {
+    //                 console.log('deleted from child tables')
+    //             }
+    //         })
+    //     }
+    //     connection.release()
+    // })
 
     mypool.getConnection((err, connection) => {
         if(err) {
