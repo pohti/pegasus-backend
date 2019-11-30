6745ad60f606d755242b700aeab0543415b7c1ea
Poh <minmarnoo@gmail.com>
2019-11-30 11:30:05 +0800

    heroku

--- api/routes/admin/deletejob.js
+++ api/routes/admin/deletejob.js
@@ -11,6 +11,28 @@ const deletejob = (req,res) => {
             throw err
         }
         else {
+            
+            let queryString1 = `DELETE FROM pegasus.jobapplication WHERE jobid=${id}`
+            connection.query(queryString1, (err) => {
+                if(err) {
+                    res.status(500).json({ message: err })
+                }
+                else {
+                    console.log('deleted from child tables')
+                }
+            })
+        }
+        connection.release()
+    })
+
+    mypool.getConnection((err, connection) => {
+        if(err) {
+            connection.release()
+            console.log(`Error getting mysql_pool connection: ${err}`)
+            throw err
+        }
+        else {
+            
             let queryString = `DELETE FROM pegasus.job WHERE id=${id}`
             connection.query(queryString, (err) => {
                 if(err) {
@@ -23,6 +45,7 @@ const deletejob = (req,res) => {
         }
         connection.release()
     })
+    
 }
 
 module.exports = deletejob
